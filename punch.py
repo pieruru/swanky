fb8decfebc9e7aaf0cf970823c624165a70b8f3d
pieruru <hellosouchi@gmail.com>
2016-10-17 13:43:05 -0400

    punch.py

--- punch.py
+++ punch.py
@@ -8,11 +8,11 @@ class Mycog:
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
+    async def punch(self, user : discord.Member):
+        """i will puch anyone!!"""
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
